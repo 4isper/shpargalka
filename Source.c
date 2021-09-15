240e2a44f739962cc2988a2820c61c5938c57a25
4isper <90023626+4isper@users.noreply.github.com>
2021-09-15 10:40:09 +0300

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void main()
+{
+	int a;
+	int b = 10;
+	char c = 1;
+	char q;
+	short w = -99;
+	short e;
+	unsigned char r = 255;
+	unsigned char y;
+	unsigned short t = 65535;
+	unsigned short u;
+	unsigned int i = -42141;
+	unsigned int o;
+	long p = -31314141;
+	long s;
+	unsigned long f = 45;
+	unsigned long g;
+	float h = 48.3;
+	float 
+
+
+	g = 5432;
+	s = -441;
+	o = 31314141;
+	y = 244;
+	u = 67;
+	e = -9;
+	a = 0;
+	q = 5;
+
+	printf("%d %d", a, b);
+}
